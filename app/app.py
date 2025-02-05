6ee584c656bf311337978153743f70fb4ded0661
hagai211 <hagai4800@gmail.com>
2025-02-05 13:26:24 +0200

    Its working!

--- app/app.py
+++ app/app.py
@@ -32,28 +32,27 @@ counter = 0
 def index():
     global counter
     counter += 1
-    internal_ip = socket.gethostbyname(socket.gethostname())
+    container_name = socket.gethostname()  # Use container name instead of IP
     client_ip = request.remote_addr
     date_time = datetime.datetime.now()
 
-    # Log the incoming request details
-    logging.info(f"Received request from {client_ip}. Processed by container IP: {internal_ip}")
+    logging.info(f"Received request from {client_ip}. Processed by container: {container_name}")
 
     cursor = mysql.connection.cursor()
     cursor.execute("INSERT INTO access_log (date_time, client_ip, internal_ip) VALUES (%s, %s, %s)",
-                   (date_time, client_ip, internal_ip))
+                   (date_time, client_ip, container_name))
     mysql.connection.commit()
     cursor.close()
 
     # Check if the cookie already exists
     existing_cookie = request.cookies.get('internal_ip')
 
-    # Set the cookie only if it doesn't exist
     if not existing_cookie:
-        response = make_response(f"Internal IP: {internal_ip}")
-        response.set_cookie('internal_ip', internal_ip, max_age=300)  # Cookie for 5 minutes
+        response = make_response(f"Container: {container_name}")
+        response.set_cookie('internal_ip', container_name, max_age=300)  # Set cookie to container name
     else:
-        response = make_response(f"Internal IP: {existing_cookie}")
+        response = make_response(f"Container: {existing_cookie}")
+
     return response
 
 
@@ -64,4 +63,3 @@ def show_count():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)  # Make sure it's accessible from all IPs
-
